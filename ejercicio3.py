54f1c70299d6011ccf007b65979feaab85ff6110
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-24 08:54:17 +0100

    funcion retirar

--- ejercicio3.py
+++ ejercicio3.py
@@ -8,3 +8,18 @@ class Cuenta:
         self.numero = numero
         self.saldo = saldo
         self.modo = modo
+
+    def retiro(self):
+        dretirado = int(input("Ha seleccionado usted la opción de retirar dinero de su cuenta.\n ¿Cuánto dinero desea retirar?:\n"))
+        if 0 < dretirado <= self.saldo:
+            self.saldo = self.saldo - dretirado
+            print("Ha retirado " + str(dretirado) + "€ y su saldo actualmente es de " + str(self.saldo) + "€.")
+            decision = int(input("¿Desea continuar realizando operaciones?(Sí = 1): "))
+            if decision == 1:
+                operacion()
+            else:
+                exit()
+
+        else:
+            print("Usted no puede retirar más dinero del que contiene el saldo de su cuenta, por favor, introduzca correctamente la cantidad a retirar de su cuenta.")
+            Cuenta.retiro(A)
